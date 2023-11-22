295d67ebfb437b51f64ee72747e78e48f3967634
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-11-21 22:48:54 -0300

    Correção na aplicação do container kafka

---
+++ projetos-exemplos/spring/01-produtor-consumidor/service--kafka-producer/src/main/java/br/com/roberto/servicekafkaproducer/ServiceKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.roberto.servicekafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ projetos-exemplos/spring/01-produtor-consumidor/service--kafka-producer/src/test/java/br/com/roberto/servicekafkaproducer/ServiceKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roberto.servicekafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
